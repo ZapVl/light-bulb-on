a4ae13fdd81e7d2e0e52cd88321b19849bdd33dd
unknown <vladislavpreriy@gmail.com>
2023-01-22 03:23:21 +0300

    animatable css

---
+++ JS/light.js
@@ -0,0 +1,8 @@
+
+let body = document.querySelector('body')
+let audio = document.querySelector('#audio')
+
+function f1() {
+    body.classList.toggle('on')
+}
+document.querySelector('.btn').onclick = f1
